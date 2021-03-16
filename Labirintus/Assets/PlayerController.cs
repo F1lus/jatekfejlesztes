f73dd5bf17d91a5dea6f90b7d25cadc126ce1b50
F1lus <48390306+F1lus@users.noreply.github.com>
2021-03-16 20:07:44 +0100

    Mozgás és pontszerzés
    Mozgás újradolgozva, plusz pontszerzési lehetőségek elszórva a mapon

--- Labirintus/Assets/PlayerController.cs
+++ Labirintus/Assets/PlayerController.cs
@@ -1,35 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class PlayerController : MonoBehaviour
 {
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
+
+    private bool canpickup;
+    public GameObject ObjectIwantToPickUp;
+
+    public int pickupCount;
+    public Text pointsHUD;
+
+    public Text finishHUD;
+
     CharacterController characterController;
-    public float MovementSpeed =2;
-    public float Gravity = 9.8f;
-    private float velocity = 0;
- 
-    private void Start()
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
+
+    void Start()
     {
         characterController = GetComponent<CharacterController>();
+
+        canpickup = false;
+        pickupCount = 0;
+        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
- 
+
     void Update()
     {
-        // player movement - forward, backward, left, right
-        float horizontal = Input.GetAxis("Horizontal") * MovementSpeed;
-        float vertical = Input.GetAxis("Vertical") * MovementSpeed;
-        characterController.Move((Vector3.right * horizontal + Vector3.forward * vertical) * Time.deltaTime);
- 
-        // Gravity
-        if(characterController.isGrounded)
+        pointsHUD.text = "Szerzett pontok: "+pickupCount;
+        
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            velocity = 0;
+            moveDirection.y = jumpSpeed;
         }
         else
         {
-            velocity -= Gravity * Time.deltaTime;
-            characterController.Move(new Vector3(0, velocity, 0));
+            moveDirection.y = movementDirectionY;
         }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+
+        if(canpickup){
+            ObjectIwantToPickUp.GetComponent<Renderer>().enabled = false;
+            ObjectIwantToPickUp.GetComponent<BoxCollider>().enabled = false;
+
+            ++pickupCount;
+
+            canpickup = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "coins")
+        {
+            canpickup = true;  
+            ObjectIwantToPickUp = other.gameObject;
+
+        }else if(other.gameObject.tag == "finishline"){
+            finishHUD.text = "Labirintus teljesítve!";
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        canpickup = false;
+     
     }
 }
